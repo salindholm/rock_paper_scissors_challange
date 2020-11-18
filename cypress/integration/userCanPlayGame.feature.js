d5bb19936282b60678be8417747ed5f7012c7599
Sanne <sannelindholm@hotmail.com>
2020-11-18 19:17:21 +0100

    Adding test for user to play game

---
+++ cypress/integration/userCanPlayGame.feature.js
@@ -0,0 +1,30 @@
+describe("User can play game against the computer", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("user choose Rock", () => {
+    cy.get("button#rock").should("contain", "Rock")
+  });
+
+  it("user choose Paper", () => {
+    cy.get("button#paper").should("contain", "Paper")
+  });
+
+  it("user choose Scissors", () => {
+    cy.get("button#scissors").should("contain", "Scissors")
+  });
+
+  it("user can see computers choice", () => {
+    cy.get("computer-play").should("contain", "Computer choise")
+  });
+
+  it("user can see the results", () => {
+    cy.get("#results").should("contain", "You win!")
+  });
+});
+
+
+
+
+
