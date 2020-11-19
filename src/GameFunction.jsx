6c2418f2f1930ca61243527d02c61dfc69c17979
Sanne <sannelindholm@hotmail.com>
2020-11-19 20:21:50 +0100

    Adding game logic

--- src/GameFunction.jsx
+++ src/GameFunction.jsx
@@ -6,6 +6,20 @@ class GameFunction extends Component {
     computer: ""
   };
 
+  decideResult = (user, computer) => {
+    if (user === computer) {
+      return "Its a tie!";
+    } else if (
+      (user === "Rock" && computer === "Scissor") ||
+      (user === "Scissor" && computer === "Paper") ||
+      (user === "Paper" && computer === "Rock")
+    ) {
+      return "You win!";
+    } else {
+      return "Computer win!";
+    }
+  };
+  
 
   render() {
     return (
