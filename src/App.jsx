00bbffb9dbd825217af23bde7eaa0b8f14013d7a
Sanne <sannelindholm@hotmail.com>
2020-11-23 22:02:39 +0100

    Adding game variable

--- src/App.jsx
+++ src/App.jsx
@@ -12,41 +12,44 @@ const Button = (props) => {
 };
 
 function App() {
-  const [setGame] = useState({
+  const [game, setGame] = useState({
   userSelection: "",
   computerSelection: "",
+  message: ""
 });
 
 const playGame = (e) => {
   const user = e.target.parentNode.getAttribute("value")
   const computer = ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
 
-if (user === computer) {
-  setGame({
-  message: (game.message = "It's a tie!"),
-  });
-} else if (
-  (user === "Rock" && computer === "Scissors") ||
-  (user === "Paper" && computer === "Rock") ||
-  (user === "Scissors" && computer === "Paper")
-) {
-  setGame({
-  message: (game.message = "You won!"),
-  });
-} else {
-  setGame({
-  message: (game.message = "You lost!"),
+  if (user === computer) {
+    setGame({
+    ...game,
+    message: (game.message = "It's a tie!"),
+    });
+  } else if (
+    (user === "Rock" && computer === "Scissors") ||
+    (user === "Paper" && computer === "Rock") ||
+    (user === "Scissors" && computer === "Paper")
+  ) {
+    setGame({
+    ...game,
+    message: (game.message = "You won!"),
+    });
+  } else {
+    setGame({
+    ...game,
+    message: (game.message = "You lost!"),
   });
 }
-
-  setGame({
+    
+setGame({
+  ...game,
   userSelection: user,
   computerSelection: computer,
  });
+};
 
-
-
-  render() {
     return (
       <>
     <h1>Rock Paper Scissors Game</h1>
@@ -56,11 +59,10 @@ if (user === computer) {
       <Button name="Scissors" onClick={ playGame } img={scissors}/>
     </div>
       <h1>{game.userSelection === "" ? "Pick one!" : `Your choice: ${game.userSelection}`}</h1>
-        <img className="pc-selection-img" src={ game.computerSelection === "Rock" ? rock : game.computerSelection === "Paper" ? paper : scissors } alt="img"/>
         <h1>Computer chose: {game.computerSelection}</h1>
       <h1 className="message">{game.message}</h1>
     </>
-  )}
+  )
 }
 
 export default App;
