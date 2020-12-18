71f96bc977152e1e8ea1544b12f8e8dd547f25ec
Sanne <sannelindholm@hotmail.com>
2020-12-18 19:10:54 +0100

    Developing test to fit all possible outcome

--- cypress/integration/userCanPlayGame.feature.js
+++ cypress/integration/userCanPlayGame.feature.js
@@ -1,4 +1,4 @@
-describe("User can play game against the computer", () => {
+describe("User can play game against the computer who chooses Rock", () => {
 	beforeEach(() => {
 		cy.visit("/", {
 			onBeforeLoad(window) {
@@ -7,7 +7,7 @@ describe("User can play game against the computer", () => {
 		});
 	});
 
-	it("by choosing Rock", () => {
+	it("by choosing Rock and its a tie", () => {
 		cy.get('[data-cy="Rock"]').click();
 		cy.get('[data-cy="user-selection"]').should("contain", "Your choice: Rock");
 		cy.get('[data-cy="computer-selection"]').should(
@@ -17,7 +17,7 @@ describe("User can play game against the computer", () => {
 		cy.get('[data-cy="result-message"]').should("contain", "It's a tie!");
 	});
 
-	it("by choosing Paper", () => {
+	it("by choosing Paper and wins", () => {
 		cy.get('[data-cy="Paper"]').click();
 		cy.get('[data-cy="user-selection"]').should(
 			"contain",
@@ -30,7 +30,7 @@ describe("User can play game against the computer", () => {
 		cy.get('[data-cy="result-message"]').should("contain", "You won!");
 	});
 
-	it("by choosing Scissors", () => {
+	it("by choosing Scissors and loses", () => {
 		cy.get('[data-cy="Scissors"]').click();
 		cy.get('[data-cy="user-selection"]').should(
 			"contain",
@@ -41,5 +41,97 @@ describe("User can play game against the computer", () => {
 			"Computer chose: Rock"
 		);
 		cy.get('[data-cy="result-message"]').should("contain", "You lost!");
+	});
+});
+
+describe("User can play game against the computer who chooses Paper", () => {
+	beforeEach(() => {
+		cy.visit("/", {
+			onBeforeLoad(window) {
+				cy.stub(window.Math, "floor").returns(1);
+			},
+		});
+	});
+
+	it("by choosing Rock and loses", () => {
+		cy.get('[data-cy="Rock"]').click();
+		cy.get('[data-cy="user-selection"]').should("contain", "Your choice: Rock");
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Paper"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "You lost!");
+	});
+
+	it("by choosing Paper and it's a tie", () => {
+		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="user-selection"]').should(
+			"contain",
+			"Your choice: Paper"
+		);
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Paper"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "It's a tie!");
+	});
+
+	it("by choosing Scissors and wins", () => {
+		cy.get('[data-cy="Scissors"]').click();
+		cy.get('[data-cy="user-selection"]').should(
+			"contain",
+			"Your choice: Scissors"
+		);
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Paper"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "You won!");
+	});
+});
+
+describe("User can play game against the computer who chooses Scissors", () => {
+	beforeEach(() => {
+		cy.visit("/", {
+			onBeforeLoad(window) {
+				cy.stub(window.Math, "floor").returns(2);
+			},
+		});
+	});
+
+	it("by choosing Rock and wins", () => {
+		cy.get('[data-cy="Rock"]').click();
+		cy.get('[data-cy="user-selection"]').should("contain", "Your choice: Rock");
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Scissors"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "You won!");
+	});
+
+	it("by choosing Paper and loses", () => {
+		cy.get('[data-cy="Paper"]').click();
+		cy.get('[data-cy="user-selection"]').should(
+			"contain",
+			"Your choice: Paper"
+		);
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Scissors"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "You lost!");
+	});
+
+	it("by choosing Scissors and it's a tie", () => {
+		cy.get('[data-cy="Scissors"]').click();
+		cy.get('[data-cy="user-selection"]').should(
+			"contain",
+			"Your choice: Scissors"
+		);
+		cy.get('[data-cy="computer-selection"]').should(
+			"contain",
+			"Computer chose: Scissors"
+		);
+		cy.get('[data-cy="result-message"]').should("contain", "It's a tie!");
 	});
 });
